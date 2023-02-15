e7d807da0865f49339b5e3172e6f382721e4d5ef
Erik Costa <erikoliveira.c@gmail.com>
2023-02-15 10:42:15 -0300

    atualização Ucontroller 10:42 - 15/02

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,6 +5,7 @@ export const contaUsuario = (req:Request, res:Response) =>{
     res.render('pages/ContaUsuario')
 }
 
+
 // export const login = (req:Request, res:Response) => {
 //     res.render('pages/login')
 // }
